28b07302d55aa6384371b06b694af66539651e1b
Lena <120247327+LenaPosh@users.noreply.github.com>
2024-01-18 20:09:02 +0400

    change RegistrationForm and signin

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import './RegistrtationForm.css'
+import './RegistrtationForm.css';
+
 function RegistrationForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -13,7 +14,7 @@ function RegistrationForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Здесь может быть код для валидации и отправки данных на сервер
+        // Validation and server submission code goes here
         console.log({ name, email, phone, password, confirmPassword });
     };
 
@@ -23,113 +24,55 @@ function RegistrationForm() {
                 <div className="form-title">Регистрация</div>
                 <form onSubmit={handleSubmit}>
                     <div>
-                        <label htmlFor="name" style={{marginBottom: '-4px'}}>Имя</label>
                         <input
                             type="text"
                             id="name"
+                            placeholder='Имя'
                             value={name}
                             onChange={(e) => handleChange(e, setName)}
                         />
                     </div>
                     <div>
-                        <label htmlFor="email" style={{marginBottom: '-4px'}}>E-mail</label>
                         <input
                             type="email"
                             id="email"
+                            placeholder='E-mail'
                             value={email}
                             onChange={(e) => handleChange(e, setEmail)}
                         />
                     </div>
                     <div>
-                        <label htmlFor="phone" style={{marginBottom: '-4px'}}>Телефон</label>
                         <input
                             type="tel"
                             id="phone"
+                            placeholder='Телефон'
                             value={phone}
                             onChange={(e) => handleChange(e, setPhone)}
                         />
                     </div>
                     <div>
-                        <label htmlFor="password" style={{marginBottom: '-4px'}}>Пароль</label>
                         <input
                             type="password"
                             id="password"
+                            placeholder='Пароль'
                             value={password}
                             onChange={(e) => handleChange(e, setPassword)}
                         />
                     </div>
                     <div>
-                        <label htmlFor="confirmPassword" style={{marginBottom: '-4px'}}>Повторите пароль</label>
                         <input
                             type="password"
                             id="confirmPassword"
+                            placeholder='Повторите пароль'
                             value={confirmPassword}
                             onChange={(e) => handleChange(e, setConfirmPassword)}
                         />
                     </div>
-                    <button type="submit">Присоединиться к работе</button>
+                    <button type="submit">Приступить к работе</button>
                 </form>
             </div>
-
         </div>
-
-
     );
 }
 
 export default RegistrationForm;
-//
-// import React, { useState } from 'react';
-// import './RegistrationForm.css';
-//
-// function RegistrationForm() {
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         email: '',
-//         phone: '',
-//         password: '',
-//         confirmPassword: '',
-//     });
-//
-//     const handleChange = (event) => {
-//         const { name, value } = event.target;
-//         setFormData(prevFormData => ({
-//             ...prevFormData,
-//             [name]: value
-//         }));
-//     };
-//
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         console.log(formData);
-//         // Здесь код для валидации и отправки данных на сервер
-//     };
-//
-//     return (
-//         <div className="page-container">
-//             <div className="form-container">
-//                 <h1 className="form-title">Регистрация</h1>
-//                 <form onSubmit={handleSubmit}>
-//                     {Object.entries(formData).map(([key, value]) => (
-//                         <div key={key}>
-//                             <label htmlFor={key}>{key}</label>
-//                             <input
-//                                 type={key === "email" ? "email" : key === "phone" ? "tel" : "text"}
-//                                 id={key}
-//                                 name={key}
-//                                 value={value}
-//                                 onChange={handleChange}
-//                                 className={value ? "has-content" : ""}
-//                             />
-//                         </div>
-//                     ))}
-//                     <button type="submit">Присоединиться к работе</button>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-//
-// export default RegistrationForm;
-
-
--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import './RegistrtationForm.css'
+import './RegistrtationForm.css';
+
 function SignInForm() {
-    // Состояния для каждого поля ввода
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -11,8 +11,8 @@ function SignInForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // код для валидации и отправки данных на сервер
-        console.log({ email, password});
+        // Validation and server submission code goes here
+        console.log({ email, password });
     };
 
     return (
@@ -20,34 +20,28 @@ function SignInForm() {
             <div className="form-container">
                 <div className="form-title">Авторизация</div>
                 <form onSubmit={handleSubmit}>
-
                     <div>
-                        <label htmlFor="email" style={{marginBottom: '-4px'}}>E-mail</label>
                         <input
                             type="email"
                             id="email"
+                            placeholder='E-mail'
                             value={email}
                             onChange={(e) => handleChange(e, setEmail)}
                         />
                     </div>
-
                     <div>
-                        <label htmlFor="password" style={{marginBottom: '-4px'}}>Пароль</label>
                         <input
                             type="password"
                             id="password"
+                            placeholder='Пароль'
                             value={password}
                             onChange={(e) => handleChange(e, setPassword)}
                         />
                     </div>
-
                     <button type="submit">Войти</button>
                 </form>
             </div>
-
         </div>
-
-
     );
 }
 
